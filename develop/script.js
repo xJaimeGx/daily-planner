0777708ddb626e88045cc93f3c26f3c093e4ea75
xJaimeGx <beanieman019@protonmail.com>
2022-03-26 14:05:11 -0500

    fix syntax, debug html and js

--- develop/script.js
+++ develop/script.js
@@ -1,16 +1,45 @@
 $(init);
 
 function init() {
-    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    // Display current day
+  $("#currentDay").text(moment().format("dddd, MMMM Do"));
+  
+   // Starts the interval to color the time block
+  blockColorSequence();
+  setInterval(blockColorSequence, 60000);
+   
+   // Update and load in local storage
+  $(".time-block").each(function() {
+    var timeBlockId = $(this).attr("id");
+    $("#" + timeBlockId + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + timeBlockId));
+  });
 
-    hourlyBlocks();
-    setInterval(hourlyBlocks, 60000);
+  $(".saveBtn").on("click", saveInput);
+};
 
-    $(".time-block").each(function() {
-        var blockInput = $(this).attr("id");
-        $("#" + blockInput + " textarea").text(localStorage.getItem(moment().format("DDDYYYY") + blockInput));
+ // Removes and adds the corresponding color for past, present, and future for each hour.
+function blockColorSequence() {
+  $(".time-block").each(function() {
+    var setHour = parseInt($(this).attr("id").replace("hour-", ""));
+    var presentHour = parseInt(moment().format("H"));
 
-    });
+    $(this).removeClass("past present future");
 
-    $(".saveBtn").on("click", handleSave);
+    if (setHour < presentHour) {
+      $(this).addClass("past");
+    } 
+    else if (setHour > presentHour) {
+      $(this).addClass("future");
+    } 
+    else {
+      $(this).addClass("present");
+    }
+  });
+};
+
+ // Save data in local storage
+function saveInput(event) {
+  var hourId = $(this).parent().attr("id");
+  localStorage.setItem(moment().format("DDDYYYY") + hourId, 
+  $("#" + hourId + " textarea").val());
 };
